41e1970713df0438543ddb79a053bf43d90c7bb0
Julie Naalsund <Julie.Naalsund@moller.no>
2022-07-08 11:30:17 +0000

    Merged PR 1222: refactoring

---
+++ python_playground/change_failure_rate.py
@@ -0,0 +1,70 @@
+from itertools import count
+from deployment_frequency import fill_working_days, get_release_per_day, get_release_per_week
+from utils import Color, get_back_track_months
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def change_failure_rate_category(total_rate):
+    if (total_rate < 1.15):
+        return ('0 - 15%', Color.green.value)
+    elif (total_rate < 1.45):
+        return ('16 - 45%', Color.yellow.value)
+    else:
+        return ('> 45%', Color.red.value)
+
+def plot_change_failure_rate(change_failure_rate_by_week, total_rate, axes):
+    week = change_failure_rate_by_week['week_start_date']
+    count = change_failure_rate_by_week['count']
+
+    mask = np.isfinite(count)
+    line, = axes[0].plot(week[mask], count[mask], ls="--", lw=1)
+    axes[0].plot(week, count, color=line.get_color(), lw=1.5)
+    axes[0].tick_params(labelrotation=45)
+    axes[0].set_title(f"Change Failure Rate")
+    axes[0].set_xlabel("Deployment Date")
+    axes[0].set_ylabel("Failure rate per week")
+
+    failure_rate_var, failure_rate_color = change_failure_rate_category(total_rate)
+    circle = plt.Circle((0, 0), 1, color=failure_rate_color)
+    axes[1].add_patch(circle)
+    axes[1].set_xlim([-1, 1])
+    axes[1].set_ylim([-1, 1])
+    axes[1].text(0.5,0.5,failure_rate_var,horizontalalignment='center',
+     verticalalignment='center', transform = axes[1].transAxes, fontsize=14)
+    axes[1].axis('off')
+    axes[1].set_box_aspect(1)
+    axes[1].set_title("Change Failure Rate")
+
+
+def change_failure_rate(pr_data, build_data, back_track_months, axes):
+    
+    #pd.set_option("display.max_rows", None, "display.max_columns", None, "display.max_colwidth", None)
+    pd.set_option("display.max_rows", None)
+    pr_data = get_back_track_months(pr_data, pr_data.creationDate, back_track_months).sort_values("closedDate")
+    build_data = get_back_track_months(build_data, build_data.queueTime, back_track_months).sort_values("queueTime")
+
+    total_rate = pr_data.shape[0]/build_data.shape[0]
+
+    merged_pr_build_data = pd.merge_asof(pr_data, build_data, left_on="closedDate", right_on="queueTime", direction="forward").sort_values("closedDate", ascending=False)
+    merged_pr_build_data = merged_pr_build_data[merged_pr_build_data.title.str.contains("fix|bug|error|crash|issue", case=False)]
+    merged_pr_build_data['date'] =  pd.to_datetime(merged_pr_build_data['finishTime']).dt.date
+    merged_pr_build_data = fill_working_days(merged_pr_build_data, back_track_months)
+    merged_pr_build_data['week_start_date'] =  pd.to_datetime(merged_pr_build_data['date']).dt.to_period('W').apply(lambda r: r.start_time)
+    merged_pr_build_data = merged_pr_build_data[["week_start_date", "finishTime", "title", "queueTime"]].sort_values("week_start_date")
+    merged_pr_build_data = merged_pr_build_data.drop_duplicates()
+    merged_pr_build_data["count"] = 1
+    
+
+    merged_pr_build_data = (merged_pr_build_data.groupby("week_start_date").sum()-1)
+
+    build_data_all = fill_working_days(build_data, back_track_months)
+    builds_per_day = get_release_per_day(build_data_all)
+    builds_per_week = get_release_per_week(builds_per_day)
+
+    all_deploys_and_fails = merged_pr_build_data.merge(builds_per_week, left_on='week_start_date', right_on='week_start_date')
+    change_failure_rate_df = all_deploys_and_fails
+    change_failure_rate_df["count"] = all_deploys_and_fails['count_x'].div(all_deploys_and_fails['count_y'].values)
+    plot_change_failure_rate(change_failure_rate_df, total_rate, axes)
+
+    return total_rate
---
+++ python_playground/main.py
@@ -0,0 +1,83 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+import pandas as pd
+from change_failure_rate import change_failure_rate
+from lead_change_time import lead_change_time
+from time_to_restore_service import time_to_restore_service
+from utils import get_data, get_ticket_data
+from urls import get_build_url, get_jira_ticket_url, get_pr_url
+from deployment_frequency import calculate_deployment_frequency
+import matplotlib.pyplot as plt
+
+
+def main():
+    pd.options.mode.chained_assignment = None 
+    env_path = Path('..', '.env')
+    load_dotenv(dotenv_path=env_path)
+    global CONFIG
+    if True:
+        CONFIG = {
+            'branch': "refs/heads/master",
+            'repo_id': 65,
+            'repo_name': 'car-care-web',
+            'json': '../car-care-web-jenkins.json',
+            'project': "mollerdigital/carcare",
+            'token': os.environ["AZURE_TOKEN"],
+            'back_track_months': 3,
+            'jira_project': 'mollermobilitygroup',
+            'jira_token': os.environ["JIRA_TOKEN"]
+        }
+    else:
+        CONFIG = {
+            'branch': "refs/heads/main",
+            'repo_id': 82,
+            'repo_name': 'workshop-booking',
+            'project': "mollerdigital/carcare",
+            'json': None,
+            'token': os.environ["AZURE_TOKEN"],
+            'back_track_months': 3
+        }
+    
+    size = 1
+    
+    plt.rcParams["figure.figsize"] = [16*size, 9*size]
+    plt.rcParams["figure.autolayout"] = True
+    
+    fig = plt.figure(constrained_layout=True)
+    fig.suptitle(f"{CONFIG['project']}/{CONFIG['repo_name']}", weight="bold")
+    gs = fig.add_gridspec(4, 5)
+
+    # GET DATA
+    build_data = get_data(get_build_url(CONFIG['project'], CONFIG['repo_id'], CONFIG['branch']), CONFIG['token'], CONFIG['json'])
+
+    build_data["queueTime"] = pd.to_datetime(build_data['queueTime']).dt.tz_localize(None)
+    build_data["startTime"] = pd.to_datetime(build_data['startTime']).dt.tz_localize(None)
+    build_data["finishTime"] = pd.to_datetime(build_data['finishTime']).dt.tz_localize(None)
+    
+    pr_data = get_data(get_pr_url(CONFIG['project'], CONFIG['repo_name'], CONFIG['branch']), CONFIG['token'])
+    pr_data = pd.DataFrame(pr_data["value"])
+    pr_data["creationDate"] = pd.to_datetime(pr_data['creationDate']).dt.tz_localize(None)
+    pr_data["closedDate"] = pd.to_datetime(pr_data['closedDate']).dt.tz_localize(None)
+    
+    
+    deployment_frequency_score = calculate_deployment_frequency(build_data, CONFIG['back_track_months'], (fig.add_subplot(gs[0:2, 3:]), fig.add_subplot(gs[1, 2])))
+    lead_change_time_median, lead_change_time_std = lead_change_time(pr_data, build_data, CONFIG['back_track_months'], (fig.add_subplot(gs[0:2, :2]), fig.add_subplot(gs[0, 2])))
+    change_failure_rate_score = change_failure_rate(pr_data, build_data, CONFIG['back_track_months'], (fig.add_subplot(gs[2:5, :2]), fig.add_subplot(gs[2, 2])))
+    time_to_restore_service_median = time_to_restore_service(pr_data, build_data, CONFIG['back_track_months'], ticket_project=CONFIG['jira_project'], axes=(fig.add_subplot(gs[2:5, 3:]), fig.add_subplot(gs[3, 2])))
+    
+    print(f"Deployment frequency: {deployment_frequency_score}")
+    print(f"Lead change time: \n Median: {lead_change_time_median}\n STD: {lead_change_time_std}")
+    print(f"Change failure rate: {change_failure_rate_score}")
+    print(f"Time to restore servive: {time_to_restore_service_median}")
+    
+    plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.4)
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
